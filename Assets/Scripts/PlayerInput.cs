dbc8236254db0350836a510421d59254a02a0bb2
J_m322 <j_m322@student.usc.edu.au>
2025-04-26 14:55:32 +1000

    Imported Unity Inheritance Package
    Imported Unity Package: Task1C_Inheritance. 3 Conflict warnings with MoveConstantly Script, PlayerInput Script & PlayerMovement Script.

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,48 +1,88 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// PlayerInput handles all of the player specific input behaviour, and passes the input information
+/// to the appropriate scripts.
+/// </summary>
 public class PlayerInput : MonoBehaviour
 {
 
-    // Create private ref to named scripts
-    private PlayerMovementScript playerMovementScript;
+    // local references
+    private PlayerMovement playerMovement;
 
-    private ShootingScript shootingScript;
-
-
-    // Start is called before the first frame update
-    void Start()
+    private WeaponBase weapon;
+    public WeaponBase Weapon
     {
-        playerMovementScript = GetComponent<PlayerMovementScript>();
+        get
+        {
+            return weapon;
+        }
 
-        shootingScript = GetComponent<ShootingScript>();
+        set
+        {
+            weapon = value;
+        }
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+        weapon = GetComponent<WeaponBase>();
+    }
+
     void Update()
     {
-        float HorizontalInput = Input.GetAxis("Horizontal");
-
-        // On input run function to move the player accordingly
-        if (HorizontalInput != 0.0f)
+        // read our horizontal input axis
+        float horizontalInput = Input.GetAxis("Horizontal");
+        // if movement input is not zero
+        if (horizontalInput != 0.0f)
         {
-            playerMovementScript.HorizontalMovement(HorizontalInput);
+            // ensure our playerMovementScript is populated to avoid errors
+            if (playerMovement != null)
+            {
+                // pass our movement input to our playerMovementScript
+                playerMovement.MovePlayer(horizontalInput * Vector2.right);
+            }
         }
 
-        // On input run function to spawn a bullet or issue a warning
+        // if we press the Fire1 button
         if (Input.GetButton("Fire1"))
         {
-            if (shootingScript != null)
+            // if our shootingScript is populated
+            if (weapon != null)
             {
-                shootingScript.FiringShots();
-            }
-
-            else
-            {
-                Debug.Log("shootingScript not attached");
+                // tell shootingScript to shoot
+                weapon.Shoot();
             }
         }
+    }
 
+    /// <summary>
+    /// SwapWeapon handles creating a new WeaponBase component based on the given weaponType. This
+    /// will popluate the newWeapon's controls and remove the existing weapon ready for usage.
+    /// </summary>
+    /// <param name="weaponType">The given weaponType to swap our current weapon to, this is an enum in WeaponBase.cs</param>
+    public void SwapWeapon(WeaponType weaponType)
+    {
+        // make a new weapon dependent on the weaponType
+        WeaponBase newWeapon = null;
+        switch (weaponType)
+        {
+            case WeaponType.machineGun:
+                newWeapon = gameObject.AddComponent<WeaponMachineGun>();
+                break;
+            case WeaponType.tripleShot:
+                newWeapon = gameObject.AddComponent<WeaponTripleShot>();
+                break;
+        }
+
+        // update the data of our newWeapon with that of our current weapon
+        newWeapon.UpdateWeaponControls(weapon);
+        // remove the old weapon
+        Destroy(weapon);
+        // set our current weapon to be the newWeapon
+        weapon = newWeapon;
     }
 }
